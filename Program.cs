f42db50c5311051504d6f4ce9f07acb0dadbeb3b
Brian Glassanos <brglassan@gmail.com>
2021-05-14 10:44:48 -0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,12 +1,43 @@
-﻿using System;
+using System;
 
-namespace Codacademy_MageOOP
+namespace MagicalInheritance
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Storm[] storms = new Storm[10];
+            int stormsIndex = 0;
+
+            Pupil p = new Pupil("Mezil-Kree", "Icecrown");
+
+            storms[stormsIndex] = p.CastWindStorm();
+            stormsIndex++;
+
+            Mage m = new Mage("Gul'dan", "Draenor");
+
+            Console.WriteLine(m.Origin);
+
+            storms[stormsIndex] = m.CastRainStorm();
+            stormsIndex++;
+
+            Archmage a = new Archmage("Nielas Aran");
+
+            storms[stormsIndex] = a.CastWindStorm();
+            stormsIndex++;
+
+            storms[stormsIndex] = a.CastRainStorm();
+            stormsIndex++;
+
+            storms[stormsIndex] = a.CastLightningStorm();
+            stormsIndex++;
+
+            for (int i = 0; i < stormsIndex; i++)
+            {
+                // storms[0]
+                Console.WriteLine(storms[i].Announce());
+            }
+
         }
     }
 }
