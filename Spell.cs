955af37d0b9c50c422ab100a04b3f0c7e833c6a7
Brian Glassanos <brglassan@gmail.com>
2021-05-14 10:46:07 -0700

    Update Spell.cs

---
+++ Spell.cs
@@ -0,0 +1,20 @@
+// Spell.cs
+using System;
+
+namespace MagicalInheritance
+{
+    abstract class Spell
+    {
+        public string Essence
+        { get; protected set; }
+
+        public bool IsStrong
+        { get; protected set; }
+
+        public string Caster
+        { get; protected set; }
+
+        public abstract string Announce();
+    }
+
+}
